03fd90febd2f1ec6cb1d7b04847609b622f937b0
maeda <maeda.han@gmail.com>
2020-04-03 03:25:00 +0400

    screenshots

---
+++ README.md
@@ -0,0 +1,2 @@
+to test, run in terminal:
+$static-server
--- js/controller/page_controller.js
+++ js/controller/page_controller.js
@@ -1,62 +1,20 @@
 /**
  * Created by mh4047 on 4/28/15.
  */
-var app = angular.module('maeda_app',[]);
-app.directive('keypressEvents',
 
-    function ($document, $rootScope) {
-        return {
-            restrict: 'A',
-            link: function () {
-                console.log('linked');
-                $document.bind('keypress', function (e) {
-                    $rootScope.$broadcast('keypress', e, String.fromCharCode(e.which));
-                });
-            }
-        }
-    });
+ /*var currentPage = $("nav")[0];*/
 
-app.controller('page_controller', function($rootScope, $scope, $location, $window){
+function loadPage(link){
+}
 
-    $scope.show_progress = false;
-    $scope.navClass = function (page, isShowProgressBar) {
-        $scope.partial_link = page;
-
-        if(isShowProgressBar) {
-            $scope.show_progress = true;
+function setPage(link, index){
+    $(".nav .button").removeClass("clicked");
+    $(".nav .button:eq(" + index+ ")").addClass("clicked");
+    $("#dynamic-content" ).load( link );
+}
 
 
-            setTimeout(function () {
-                $scope.$apply(function () {
-                    $scope.show_progress = false;
-                    console.log('done')
-                })
-            }, 1000);
-            console.log("loading")
-        }
-    };
 
-    $scope.go = function ( path ) {
-      $window.location.href = path;
-    };
-
-    $scope.key = "";
-    $scope.obj = {privacyon: false};
-
-    $rootScope.$on('keypress', function (evt, obj, key) {
-        $scope.key = $scope.key +key;
-        if(_.contains($scope.key, "hide")){
-            $scope.key = "";
-            $scope.obj.privacyon = true;
-            console.log($scope.key)
-            $scope.$apply()
-        }else if(_.contains($scope.key, "show")){
-            $scope.key = "";
-            $scope.obj.privacyon = false;
-            console.log($scope.key)
-            $scope.$apply()
-        }
-
-    })
-
-})
+$(document).ready(function(){
+    setPage("partial/projects.html",0);
+});
