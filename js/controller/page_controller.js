8f3262030c970d0a1f37000a58ca5c1499434c6f
maeda <maeda.han@gmail.com>
2020-09-21 10:36:25 +0400

    updates

--- js/controller/page_controller.js
+++ js/controller/page_controller.js
@@ -2,16 +2,28 @@
  * Created by mh4047 on 4/28/15.
  */
 
- /*var currentPage = $("nav")[0];*/
 
 function loadPage(link){
+}
+
+function setThumbnailRedirects(){
+	$('.thumbnail img').click(function(ev){
+	    src = jQuery(this).attr("src");
+	    redirect = src.replace("thumbnails/","img/")
+	    //load non-thumbnail version on click a thumbnail
+	    window.location.href = redirect;
+	});
 }
 
 function setPage(link, index){
     $(".nav .button").removeClass("clicked");
     $(".nav .button:eq(" + index+ ")").addClass("clicked");
-    $("#dynamic-content" ).load( link );
+    $("#dynamic-content" ).load( link, function(){
+    	setThumbnailRedirects();
+    } );
+    
 }
+
 
 
 
@@ -19,3 +31,5 @@ $(document).ready(function(){
     setPage("partial/projects.html",0);
     //setPage("partial/cv.html",1);
 });
+
+
